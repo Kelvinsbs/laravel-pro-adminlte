2be3764e384557ad05ee93f0b73e52d0a8fb090f
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-01-29 13:09:20 -0300

    listagem usuarios parte 1

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.default')
+@section('page-title', 'Usuários')
+@section('content')
+    Lista de USuarios
+{{-- <div class="container">
+    <h1>Listagem de Usuários</h1>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>
+                    <a href="{{ route('users.show', $user->id) }}" class="btn btn-info">Ver</a>
+                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning">Editar</a>
+                    <form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Excluir</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div> --}}
+@endsection
