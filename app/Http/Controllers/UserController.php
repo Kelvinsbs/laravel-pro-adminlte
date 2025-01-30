c72e21b8d39e6a89eadff2b5eb7b776cb24fa9a3
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-01-30 13:04:47 -0300

    Add usuarios parte 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,13 @@ class UserController extends Controller
         $users = User::all();
         return view('users.index', compact('users'));
     }
+
+    public function create() {
+        return view('users.create');
+    }
+
+    public function store(Request $request) {
+        dd($request);
+
+    }
 }
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+@section('page-title', 'Adicionar Usuário')
+@section('content')
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" aria-describedby="name" value="{{ old('name') }}">
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
+@endsection
